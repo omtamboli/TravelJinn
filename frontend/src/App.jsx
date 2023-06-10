c8213fc7eda51ddd8428a16cd0a80083526097ef
omtamboli <omtamboli08@gmail.com>
2023-06-10 16:45:55 +0530

    flight search functionality done !

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import FlightBooking from '../src/Pages/FlightBooking/FlightBooking';
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Home from "./Pages/Home/Home";
 function App() {
   return (
     <>
@@ -8,6 +9,7 @@ function App() {
         <Router>
           <Routes>
             <Route path="/flightbook" element={<FlightBooking />} />
+            <Route path="/" element={<Home />} />
           </Routes>
         </Router>
       </div>
